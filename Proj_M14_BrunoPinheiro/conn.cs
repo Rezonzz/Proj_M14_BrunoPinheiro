8708c08f1eca2255008002fc2608fc65889dfee2
Bruno Pinheiro <95320065+Rezonzz@users.noreply.github.com>
2023-03-19 21:39:05 +0000

    Blue Lock

---
+++ Proj_M14_BrunoPinheiro/conn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Proj_M14_BrunoPinheiro
+{
+    internal class conn
+    {
+        private static readonly string connectionString = "server=localhost;userid=root;database=clube_desportivo_brunopinheiro;password=";
+
+        public static MySqlConnection GetConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
